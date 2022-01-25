8dce16a3cc572ca93c8b430ec6f24839a2c009ba
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2022-01-25 13:24:13 +0700

    Memperbaiki tampilan bagian Skills

--- js/script.js
+++ js/script.js
@@ -82,7 +82,7 @@ $(window).on('load', function() {
 
 
 
-// Portfolio
+// skills
 // .scroll = event untuk menangkap pada saat halaman di scroll
 $(window).scroll(function() {
     // membuat variabel bernama wScroll = cari elemen di sini kemudian tangkap scrolltopnya
@@ -109,13 +109,13 @@ $(window).scroll(function() {
         'transform': 'translate(0px,'+wScroll/1.5+'%)'
     });
 
-    // portfolio
-    if(wScroll > $('.portfolio').offset().top - 250) {
+    // skills
+    if(wScroll > $('.skills').offset().top - 250) {
         // console.log('OK');
         
-        // each = mengambil setiap elemen yang ada pada class thumbnail dalam class portfolio
+        // each = mengambil setiap elemen yang ada pada class thumbnail dalam class skills
         // i = index, misal di dalam thumbnail ada 6 elemen maka indexnya ada 6 (1-6) (index dimulai dari 0)
-        $('.portfolio .thumbnail').each(function(i){
+        $('.skills .thumbnail').each(function(i){
             // setTimeout = memberikan waktu untuk menunggu untuk menjalankan sesuatu yang ada di dalamnya
             // di dalam setTimeout akan ada fungsi
             // setelah fungsi tambahkan ',' lalu isi waktu tunggunya. Misalnya 2000 (2 detik)
@@ -124,7 +124,63 @@ $(window).scroll(function() {
                 
                 // eq = untuk menentukan elemen yang keberapa
                 /* addClass = untuk menambahkan class */
-                $('.portfolio .thumbnail').eq(i).addClass('muncul');
+                $('.skills .thumbnail').eq(i).addClass('muncul');
+
+            // 300 * (i+1) = waktu 300 detik berlipat untuk setiap i. misal i1 = 300, i2 = 600, i3 = 900, dst
+            // gunanya +1 = karna setiap index diawali angka 0, agar hasilnya 1 maka perlu ditambah 1
+            }, 300 * (i+1)); 
+
+
+        });
+
+        
+    }
+
+})
+
+// experience
+// .scroll = event untuk menangkap pada saat halaman di scroll
+$(window).scroll(function() {
+    // membuat variabel bernama wScroll = cari elemen di sini kemudian tangkap scrolltopnya
+    var wScroll = $(this).scrollTop();
+
+    // console.log(wScroll);
+
+    // jumbotron
+    // menangkap elemen img pada jumbotron menggunakan jQuery
+    // kemudian merubah fungsi css
+    // transform = properti css untuk merubah pergerakan
+    // translate = mentranslasikan
+    // (0px...) = koordinat sumbu x
+    // +wScroll/4.5+ = sumbu y mengambil dari variabel wScroll dan dibagi 4.5
+    $('.jumbotron img').css({
+        'transform': 'translate(0px,'+wScroll/5+'%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform': 'translate(0px,'+wScroll/2.2+'%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform': 'translate(0px,'+wScroll/1.5+'%)'
+    });
+
+    // experience
+    if(wScroll > $('.experience').offset().top - 250) {
+        // console.log('OK');
+        
+        // each = mengambil setiap elemen yang ada pada class thumbnail dalam class experience
+        // i = index, misal di dalam thumbnail ada 6 elemen maka indexnya ada 6 (1-6) (index dimulai dari 0)
+        $('.experience .thumbnail').each(function(i){
+            // setTimeout = memberikan waktu untuk menunggu untuk menjalankan sesuatu yang ada di dalamnya
+            // di dalam setTimeout akan ada fungsi
+            // setelah fungsi tambahkan ',' lalu isi waktu tunggunya. Misalnya 2000 (2 detik)
+            setTimeout(function() {
+                console.log('OK');
+                
+                // eq = untuk menentukan elemen yang keberapa
+                /* addClass = untuk menambahkan class */
+                $('.experience .thumbnail').eq(i).addClass('muncul');
 
             // 300 * (i+1) = waktu 300 detik berlipat untuk setiap i. misal i1 = 300, i2 = 600, i3 = 900, dst
             // gunanya +1 = karna setiap index diawali angka 0, agar hasilnya 1 maka perlu ditambah 1
